3879e6ad8bcdc21caf4f56dbc07100d102d28424
Lars Brinkhoff <lars@nocrew.org>
2023-12-15 21:49:06 +0100

    Check page numbers.

--- tools/source.sh
+++ tools/source.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+page() {
+    n=`head -1 "$1" | grep -o 'PAGE [0-9.]*$' | grep -o '[0-9.]*$'`
+    case "$n" in
+        *.?) echo "000$n" | tr -d . | tail -c5;;
+        *)   echo "000${n}0" | tail -c5;;
+    esac
+}
+
 fix() {
     case "$1" in
         *page0000.txt) return;;       #Skip "ITS 138" page.
@@ -7,6 +15,10 @@ fix() {
         *page1190.txt) exit 0;;       #Leave out symbol table.
         *page???0.txt) printf '\f';;
     esac
+    if test `basename $1 | cut -c5-8` \!= `page "$1"`; then
+        echo "Page number mismatch in $1"
+        exit 1
+    fi
     head -1 "$1" | cut -c17- | sed 's/ *PAGE [0-9.]*$//' | ./tabify
     tail -n +2 "$1" | cut -c17- | ./tabify
 }
